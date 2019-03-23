f5f403b763f88cbb4d381483a42ee55825d5e330
Shuguberu <42670107+Shuguberu@users.noreply.github.com>
2019-03-23 10:42:00 +0800

    Update 猜整数.py
    加入随机数

--- 猜整数.py
+++ 猜整数.py
@@ -1,13 +1,15 @@
+import random
+secret=random.randint(1,10)
 print("=======This is Shuguberu=======")
 temp=input("输入数字")
 guess=int(temp)
-while guess!=8:
+while guess!=secret:
     temp=input("wrong,once again:")
     guess=int(temp)
-    if guess==8:
+    if guess==secret:
         print("right")
     else:
-        if guess>8:
+        if guess>secret:
             print("大了")
         else:
             print("小了")
