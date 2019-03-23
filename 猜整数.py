dce362fd2466d79ab25061a11ba5878cad8d1bb8
Shuguberu <42670107+Shuguberu@users.noreply.github.com>
2019-03-23 10:05:30 +0800

    Update 猜整数.py

--- 猜整数.py
+++ 猜整数.py
@@ -1,7 +1,11 @@
+print("=======This is Shuguberu=======")
 temp=input("输入数字")
 guess=int(temp)
 if guess==8:
-    print("nice")
+    print("right")
 else:
-    print("wrong")
+    if guess>8:
+        print("大了")
+    else：
+        print("小了")
 print("over")
